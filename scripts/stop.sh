08bd3fd2bd009feb26ff47436b963c32b2536515
jey <code@jnf.dev>
2026-03-11 16:16:07 -0700

    replaces the 'clear oled' script with a better stop/start pair

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Turn off the OLED display and stop crust.
-sudo systemctl stop crust
 # Control byte 0x00 = command stream; 0xAE = display off
+
+mpc stop
+sudo systemctl stop cava crust
 i2ctransfer -y 1 w2@0x3c 0x00 0xae
